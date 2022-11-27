e57a6e68f43f49c4ab118dca92a610af8329ba1b
likurg42 <likurg42@gmail.com>
2022-11-27 21:30:39 +0300

    fix: fix spacing formatting after color

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -11,11 +11,11 @@ const stylish = (tree) => {
 
   const format = (data, depth) => {
     if (!_.isObject(data)) {
-      return data === '' ? '' : ` ${data}`;
+      return data;
     }
 
-    return ` {\n${Object.entries(data).map(([key, value]) => {
-      const line = `${getIndent(depth)}${key}:${format(value, depth + 1)}`;
+    return `{\n${Object.entries(data).map(([key, value]) => {
+      const line = `${getIndent(depth)}${key}: ${format(value, depth + 1)}`;
       return line;
     }).join('\n')}\n${getIndent(depth - 1)}}`;
   };
@@ -35,16 +35,16 @@ const stylish = (tree) => {
 
     switch (type) {
       case 'unchanged': {
-        return `${getIndent(depth)}${name}:${format(value, depth + 1)}`;
+        return `${getIndent(depth)}${name}: ${format(value, depth + 1)}`;
       }
       case 'changed': {
-        return `${oldValueIndent}${name}:${format(oldValue, depth + 1)}\n${newValueIndent}${name}:${format(newValue, depth + 1)}`;
+        return `${oldValueIndent}${name}: ${format(oldValue, depth + 1)}\n${newValueIndent}${name}: ${format(newValue, depth + 1)}`;
       }
       case 'added': {
-        return `${newValueIndent}${name}:${format(value, depth + 1)}`;
+        return `${newValueIndent}${name}: ${format(value, depth + 1)}`;
       }
       case 'removed': {
-        return `${oldValueIndent}${name}:${format(value, depth + 1)}`;
+        return `${oldValueIndent}${name}: ${format(value, depth + 1)}`;
       }
       case 'nested': {
         return `${getIndent(depth)}${name}: {\n${children.map((el) => iter(el, depth + 1)).join('\n')}\n${getIndent(depth)}}`;
